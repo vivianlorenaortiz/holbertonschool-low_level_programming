26dbb1b9ae27cf0d12b9a568f579d16b4a689eee
vivianlorenaortiz <vivianlorena0414@gmail.com>
2019-06-19 22:02:52 +0000

    change the command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#incluide<stdio.h>
+#include<stdio.h>
 /**
  * main - Entry point
  *
@@ -8,6 +8,4 @@ int main(void)
 {
 	puts(“\"Programming is like building a multilingual puzzle”);
 	return (0);
-
 }
-
