195c5384840149ff9c1f56660b81c4071adc8a48
vivianlorenaortiz <vivianlorena0414@gmail.com>
2019-06-25 14:44:22 +0000

    _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,14 +1,19 @@
 #include "holberton.h"
 
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-char n [] ="Holberton";
+char n[] = " Holberton ";
 int hol = 0;
 for (hol = 0; hol < 9; hol++)
 {
-  _putchar (n[hol]);
- }
+_putchar (n[hol]);
+}
 _putchar ('\n');
 
- return (0);
+return (0);
 }
