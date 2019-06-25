249c3323e113e602c65151016bf3aac8a247b70f
vivianlorenaortiz <vivianlorena0414@gmail.com>
2019-06-25 16:19:10 +0000

    value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *_abs- Computes the absolute value of an integer.
+ *@n: integer to computer
+ *Return: Always 0.
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+			return (n);
+}
+	else
+{
+		return (n);
+}
+}
