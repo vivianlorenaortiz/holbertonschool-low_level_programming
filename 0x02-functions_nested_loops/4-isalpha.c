019066fd5963782f386c342a3c024a8bf02e6494
vivianlorenaortiz <vivianlorena0414@gmail.com>
2019-06-25 15:35:47 +0000

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * _isalpha - check for alphabetic charter.
+ *@c: Is a integer
  * Return: Always 0.
  */
 int _isalpha(int c)
 {
 
-  if  ((c >= 97 && c<= 122) || (c >= 65 && c<= 90))
-    return (1);
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
 
-else 
-return (0); 
+	else
+		return (0);
 }
