d97017a9982d06e36e220e94d3e908137f2306a0
vivianlorenaortiz <vivianlorena0414@gmail.com>
2019-06-25 18:00:43 +0000

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ *print_last_digit - Funtion that prints the last digital of a number.
+ *@d: integer to computer
+ *Return: Always 0.
+ */
+int print_last_digit(int d)
+
+{
+	int m;
+	char x;
+
+	if (d >= '0')
+	{
+		m = (d % 10);
+			x = m + '0';
+		_putchar (x);
+	}
+	else
+	{
+		m = (d % 10) * (-1);
+				x = m + '0';
+	_putchar (x);
+	}
+	return (m);
+}
