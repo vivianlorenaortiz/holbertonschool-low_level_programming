9c3da77909a337d17a32889d3ce8946ad368d543
vivianlorenaortiz <vivianlorena0414@gmail.com>
2019-06-25 15:44:48 +0000

    Sing

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,27 +1,27 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * print_sign - print the singn of a number.
+ *@n: Is a integer
  * Return: Always 0.
  */
 int print_sign(int n)
 {
 
-if  (n > 0)
+	if  (n > 0)
 {
-    _putchar('+');
-    return (1);
- }
- else if(n == 0)
-   
-{
- _putchar('0');
-    return (0);
+	_putchar('+');
+	return (1);
 }
- else
+	else if (n == 0)
+
 {
-      _putchar('-');
-return (-1); 
+	_putchar('0');
+	return (0);
+}
+	else
+{
+	_putchar('-');
+	return (-1);
 }
 }
