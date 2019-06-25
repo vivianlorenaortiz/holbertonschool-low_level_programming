5525ab889e5ed3591c371a00f366dcfa0794c42b
vivianlorenaortiz <vivianlorena0414@gmail.com>
2019-06-25 15:22:11 +0000

    _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,12 +7,13 @@
  */
 int main(void)
 {
-char n[] = " Holberton ";
-int hol = 0;
-for (hol = 0; hol <= 9; hol++)
-{
-_putchar (n[hol]);
-}
-_putchar ('\n');
-return (0);
+	char n[] = " Holberton ";
+	int hol = 0;
+
+	for (hol = 0; hol <= 9; hol++)
+	{
+		_putchar (n[hol]);
+	}
+	_putchar ('\n');
+	return (0);
 }
