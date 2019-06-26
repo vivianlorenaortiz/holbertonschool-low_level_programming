9049bd5f626bbc4013e76e87a61d7160c390f429
vivianlorenaortiz <973@holbertonschool.com>
2019-06-26 06:25:12 +0200

    Fix commit

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -3,7 +3,8 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
+void positive_or_negative (int i);
+int largest_number (int a, int b, int c);
+int convert_day (int month, int day);
+
 #endif /* HOLBERTON_H */
