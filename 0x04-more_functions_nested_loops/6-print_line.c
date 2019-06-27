4aa4654bb89f116a35900ae0aa68a6602a694aac
vivianlorenaortiz <973@holbertonschool.com>
2019-06-27 23:57:11 +0200

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *
+ *print_line - Function thar draws a straigth line in the terminal
+ *@n: Integer to computer
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n > 0)
+	{
+		for (x = 0; x < n; x++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
