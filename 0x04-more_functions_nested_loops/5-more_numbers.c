7c3dd15db926441c47ff3bdced50705745685be3
vivianlorenaortiz <973@holbertonschool.com>
2019-06-27 23:08:14 +0200

    Numbers constitude

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,25 +1,21 @@
 #include "holberton.h"
 /**
- *more _numbers - Prints 10 times the numbers, form 0 to 14
+ *more_numbers - Prints 10 times the numbers, form 0 to 14
  *
  */
 
 void more_numbers(void)
 {
-	int i,j;
+	int i, j;
 
-	for (j = 0; j < 10; j++)
+	for (j = 0; j <= 10; j++)
 	{
-			for (i = 0; i < 15; i++)
-			{
-				if (i <= 9)
-					_putchar((i) + '0');
-				else
-				{
-					_putchar((i / 10) + '0');
-					_putchar((i % 10) + '0');
-				}
-			}
-			_putchar ('\n');
+		for (i = 0; i < 15; i++)
+		{
+			if (i > 9)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		_putchar ('\n');
 	}
 }
