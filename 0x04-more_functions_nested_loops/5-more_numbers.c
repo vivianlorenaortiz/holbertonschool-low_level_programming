2ec22c5e41d38d2c877ef27ddec5e170f05abd3d
vivianlorenaortiz <973@holbertonschool.com>
2019-06-27 23:12:54 +0200

    Numbers constitute the only universal

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,9 +8,9 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (j = 0; j <= 10; j++)
+	for (j = 0; j < 10; j++)
 	{
-		for (i = 0; i < 15; i++)
+		for (i = 0; i <= 14; i++)
 		{
 			if (i > 9)
 				_putchar((i / 10) + '0');
