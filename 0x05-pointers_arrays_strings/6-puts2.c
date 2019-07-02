1a2f9d9d06ca5e70b1149a9503c7feb5d671b20c
vivianlorenaortiz <973@holbertonschool.com>
2019-07-02 23:44:07 +0200

    funtion that prints ome char out of 2 of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * puts2 - function that prints one char out of 2 of a string
+ *@str: Integer to computer
+ *
+ */
+
+void puts2(char *str)
+
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a+=2)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
