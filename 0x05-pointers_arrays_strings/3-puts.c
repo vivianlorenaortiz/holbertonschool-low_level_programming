0992e486e1061f1f4dd2147b9c6b224a464781a8
vivianlorenaortiz <973@holbertonschool.com>
2019-07-02 21:22:37 +0200

    function that prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *_puts - function that prints a strings
+ *@str:  "I do not fear computers. I fear the lack of them - Isaac Asimov"
+ *
+ */
+
+void _puts(char *str)
+
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
