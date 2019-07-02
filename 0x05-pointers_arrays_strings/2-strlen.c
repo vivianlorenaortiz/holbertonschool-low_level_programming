80661c39b5e8b097739847c2b151aaf311f2144e
vivianlorenaortiz <973@holbertonschool.com>
2019-07-02 21:10:50 +0200

    lenghth of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *_strlen - Function that returns the length of a string
+ *@s: Integer to computer
+ *Description: return the long number
+ *
+ * Return: i
+ */
+
+int _strlen(char *s)
+
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
