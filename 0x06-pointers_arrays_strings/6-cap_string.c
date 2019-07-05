241d36f4817742d36e3bd064567836df0812db4b
vivianlorenaortiz <973@holbertonschool.com>
2019-07-05 05:23:32 +0200

    capitalizes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ *
+ *cap_string - capitalizes all words
+ *@s: pointer
+ *Return: the string
+ */
+char *cap_string(char *s)
+{
+	int j;
+	int i = 0;
+	int sp[13] = {' ', '\n', '\t', ';', '.', '!', '?',
+		      '(', ')', '{', '}', '"', ','};
+
+	if (s[i] >= 'a' && s[i] <= 'z')
+		s[i] -= 32;
+	for (i = 1; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if ((s[i] >= 'a' && s[i] <= 'z') && (s[i - 1] == sp[j]))
+			{
+				s[i] -= 32;
+			}
+		}
+	}
+	return (s);
+}
