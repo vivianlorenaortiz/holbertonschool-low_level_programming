15ec13a8c2d9de762e6f4a43a2db08cb7c88a93d
vivianlorenaortiz <973@holbertonschool.com>
2019-07-09 00:47:40 +0200

    2 strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdlib.h>
 /**
  * _strchr -  charater in a string
  * @s: pointer
@@ -10,10 +11,13 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	if (s != NULL)
+	{
+		for (i = 0; s[i] != '\0'; i++)
 	{
 		if (*(s + i) == c)
 			return (&s[i]);
+	}
 	}
 	return ('\0');
 }
