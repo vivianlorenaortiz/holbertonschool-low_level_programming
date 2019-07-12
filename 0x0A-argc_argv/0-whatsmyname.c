4216817a3fc9726a4b7e89fcb2a44cb28cc0e0e7
vivianlorenaortiz <973@holbertonschool.com>
2019-07-13 00:50:47 +0200

    Write a program that prints its nam

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: pointer string
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		printf("%s\n", argv[0]);
+
+	}
+	return (0);
+}
