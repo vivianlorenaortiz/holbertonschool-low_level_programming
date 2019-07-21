863d91d738c3379f8ee8ce5ff68db38e54073f40
vivianlorenaortiz <973@holbertonschool.com>
2019-07-22 00:30:40 +0200

    fix exercises five

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -9,5 +9,31 @@
  */
 char *argstostr(int ac, char **av)
 {
+int s, j, k = 0, n = 0;
+	char *a;
 
+	if (ac <= 0 || av == NULL)
+		return (NULL);
+	for (s = 0; s < ac; s++)
+	{
+		for (j = 0; av[s][j]; j++)
+			n++;
+		n++;
+	}
+	n++;
+	a = malloc(n * sizeof(char));
+	if (a == NULL)
+		return (NULL);
+	for (s = 0; s < ac; s++)
+	{
+		for (j = 0; av[s][j]; j++)
+		{
+			a[k] = av[s][j];
+			k++;
+		}
+		a[k] = '\n';
+		k++;
+	}
+	a[k] = '\0';
+	return (a);
 }
