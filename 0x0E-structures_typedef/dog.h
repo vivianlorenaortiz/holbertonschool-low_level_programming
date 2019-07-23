a4745e5aa0535737a8b194b751094a6488a619e1
vivianlorenaortiz <973@holbertonschool.com>
2019-07-23 05:15:19 +0200

    Fix commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,10 @@
 #ifndef _DOG_H_
 #define _DOG_H_
-
-/**
- *dog_t - typedef for struct dog
- */
-
-typedef struct dog dog_t;
-
 /**
  *struct dog - struct for information of a dog.
  *@name: name of the dog
  *@age: age of the dog
  *@owner: owner of the dog.
- *
- *
  *Description:  struct called "dog"
  */
 struct dog
@@ -23,9 +14,14 @@ struct dog
 	char *owner;
 };
 
+/**
+ *dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif
+#endif /* _DOG_H_ */
