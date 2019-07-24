702ed50aa2f73f6f441b39654280fcf73c18c665
vivianlorenaortiz <973@holbertonschool.com>
2019-07-24 23:52:39 +0200

    fix if condition

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,8 +11,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if ((array != NULL && size <= 0) || cmp != NULL)
-	for (i = 0; i < size; i++)
+	if (size <= 0|| array != NULL || cmp != NULL)
+
+		for (i = 0; i < size; i++)
 		if (cmp(array[i]))
 			return (i);
 	return (-1);
