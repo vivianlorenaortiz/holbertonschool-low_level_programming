97a7b0d2312ddfb0949cac7a8d57ea09b5fb7396
vivianlorenaortiz <973@holbertonschool.com>
2019-07-24 23:49:55 +0200

    int index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,6 +11,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if ((array != NULL && size <= 0) || cmp != NULL)
 	for (i = 0; i < size; i++)
 		if (cmp(array[i]))
 			return (i);
