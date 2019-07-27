4b13748b7572fa187054f23921860a2f3464b1d3
vivianlorenaortiz <973@holbertonschool.com>
2019-07-28 01:48:26 +0200

    commit

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -63,7 +63,7 @@ void print_all(const char * const format, ...)
 
 	va_start(valist, format);
 	i = 0;
-	while (format != NULL && format[i])
+	while (format && format[i])
 	{
 		a = 0;
 		while (valu[a].p != NULL)
