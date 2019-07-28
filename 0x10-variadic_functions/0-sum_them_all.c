e597a4f6e7356bc3250da09a1b930f3ed6e91b0f
vivianlorenaortiz <973@holbertonschool.com>
2019-07-28 05:36:47 +0200

    conditional

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,12 +12,14 @@ int sum_them_all(const unsigned int n, ...)
 	va_list num;
 	int sum = 0;
 	unsigned int i;
-
+	if (n == 0)
+		return (0);
 	va_start(num, n);
 
 	for (i = 0; i < n; i++)
+	{
 		sum += va_arg(num, unsigned int);
-
+	}
 	va_end(num);
 
 	return (sum);
