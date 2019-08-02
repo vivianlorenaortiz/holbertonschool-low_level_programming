a3b4d00069fd93e47602ab86463cfca694356e23
vivianlorenaortiz <973@holbertonschool.com>
2019-08-02 04:34:50 +0200

    function print

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ *torise - function print
+ *
+ *Return: void
+ */
+void torise(void) __attribute__ ((constructor));
+
+	void torise(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
