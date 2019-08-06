2821c30d57f5765c46fabc77aa798d31a1a9acec
vivianlorenaortiz <973@holbertonschool.com>
2019-08-06 17:43:32 +0200

    freelistint

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *free_listint2 - function that frees a listint.
+ *@head: double pointer to the list.
+ *Return: void.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *next;
+
+	if (head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+}
