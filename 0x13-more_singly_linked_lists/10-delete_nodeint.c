b0d33ae411cac5061af0688c43bad9f4929fe712
vivianlorenaortiz <973@holbertonschool.com>
2019-08-07 05:05:20 +0200

    adding exercise ten

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,40 +1,37 @@
 #include "lists.h"
+
 /**
- *insert_nodeint_at_index - function that insert a new node at given position.
- *@head: Pointer to the first node in the list
- *@idx: Is the index of the list where the new node should be added.
- *@n: is the nuber contains node.
- *Return: The address of the new node, or NULL if it failed
+ *delete_nodeint_at_index- function that insert a new node at given position.
+ *@head: double pointer to the first node in the list
+ *@index: index of the node to delete.
+ *
+ *Return: Pointer to the indexed node
  */
 
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int i = 0;
+	unsigned int i;
 	listint_t *current, *nw;
-	current = *head;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
-
-	for (i = 0; current != NULL; i++)
+	if (index != 0)
 	{
-		if (i == (index - 1) && index != 0)
-		{
-			nw = current->next;
-			current->next = nw->next;
-			nw->next = NULL;
-			free(nw);
-			return (1);
-		}
-		else if (index == 0)
-		{
-			nw = *head;
-			*head = nw->next;
-			nw->next = NULL;
-			free(nw);
-			return (1);
-		}
-		current = current->next;
+
+		nw = (*head)->next;
+		free(*head);
+		*head = nw;
+		return (1);
 	}
-	return (-1);
+	current = *head;
+	for (i = 0; i < index - 1; i++)
+	{
+			if (current == NULL)
+				return (-1);
+			current = current->next;
+	}
+	nw = current->next;
+	current->next = nw->next;
+	free(nw);
+	return (1);
 }
