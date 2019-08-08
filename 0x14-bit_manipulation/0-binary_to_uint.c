ba756c08a8d80218184675475cc39045530f6143
vivianlorenaortiz <973@holbertonschool.com>
2019-08-09 01:48:37 +0200

    fix chance commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,18 +1,18 @@
 #include "holberton.h"
 /**
- *binaty_to_uint - function that converts a binary number to an unsigned int.
+ *binary_to_uint - function that converts a binary number to an unsigned int.
  *@b: Is pointer to a string of 0 and 1 chars.
  *Return: The converti number, or 0 if.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned decimal = 0;
-	unsigned i = 0;
+	unsigned int decimal = 0;
+	unsigned int i = 0;
 
 	if (!b)
 		return (0);
 
-	for (i = 0; b[i] != '\0';i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
