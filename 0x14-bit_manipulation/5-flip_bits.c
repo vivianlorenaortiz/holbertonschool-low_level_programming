e74eb2b66fc273f80dcc0719e8d9bcd3a8da724e
vivianlorenaortiz <973@holbertonschool.com>
2019-08-09 04:36:38 +0200

    flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * flip_bits - Function that returns teh number of bits you would need.
+ *@n: Integer.
+ *@m: Integr.
+ *Return: count.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int mask;
+	unsigned int count = 0;
+
+	mask =	n ^ m;
+
+	while (mask)
+	{
+		if (mask & 1)
+			count++;
+		mask = mask >> 1;
+	}
+	return (count);
+}
