63659531e7cc12c1b1e1d111da0095bda26f4e99
vivianlorenaortiz <973@holbertonschool.com>
2019-08-14 07:04:10 +0200

    cp file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98); }
-	f2 = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+	f2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
 	if (f2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
