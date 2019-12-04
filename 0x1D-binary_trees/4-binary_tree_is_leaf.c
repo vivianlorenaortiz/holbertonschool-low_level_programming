723dba566d1ba3e63058019605787f3ef7fb4257
vivianlorenaortiz <973@holbertonschool.com>
2019-12-04 20:51:20 +0100

    chance in contional

--- 0x1D-binary_trees/4-binary_tree_is_leaf.c
+++ 0x1D-binary_trees/4-binary_tree_is_leaf.c
@@ -8,8 +8,11 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node->left || node->right || !node)
-		return (0);
-	else
+	if (node)
+	{
+		if (node->left || node->right || !node)
+			return (0);
 		return (1);
+	}
+	return (0);
 }
