56afd49a1cc0ce8a247dfbdc8bb5d636b5aeb720
vivianlorenaortiz <973@holbertonschool.com>
2019-12-04 21:03:42 +0100

    chance coding

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -1,5 +1,24 @@
 #include "binary_trees.h"
 /**
+* binary_tree_is_full - checks to see if this is a full tree
+* @tree: a pointer to the tree
+*
+* Return: an int
+*/
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left)
+			&& binary_tree_is_full(tree->right));
+	return (0);
+}
+
+/**
  *binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree
  *@tree: Is a pointer to the root node of the tree to count the number.
  *Return: Is tree is NULL, the function must return 0.
@@ -29,6 +48,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	int right = 0;
 
 	if (!tree)
+		return (0);
+
+	if (!(binary_tree_is_full(tree)))
 		return (0);
 
 	left = binary_tree_nodes(tree->left);
