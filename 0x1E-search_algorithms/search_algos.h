f85f4f956acecaa8387ba00cba213f578fcb22e3
vivianlorenaortiz <973@holbertonschool.com>
2020-01-15 21:40:53 +0100

    adding file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,4 +6,5 @@
 #include <stdlib.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+void printArray(int *array, size_t start, size_t end);
 #endif
